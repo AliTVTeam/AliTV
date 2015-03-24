8c1aff31bec24da8d226adafc5e276f8bd8c1f45
Sonja Hohlfeld <Sonja-Hohlfeld@gmx.de>
2015-03-24 15:55:34 +0100

    Variablenname spezifiert, jetzt passieren keine Fehler mehr, zumindest fast keine mehr...

--- wgaPipeline_jasmine/spec/testing_loadKaryo.js
+++ wgaPipeline_jasmine/spec/testing_loadKaryo.js
@@ -1,11 +1,11 @@
-var file = "data/karyo.json";
+var karyoFile = "data/karyo.json";
 describe("test the verification of file", function(){
 	it("karyo file should be defined", function(){
 		var file = "data/karyo.json";
 		expect(file).toBeDefined();
 	});
 	it("the karyo file in the function should be defined", function(){
-		expect(loadKaryoFile("data/karyo.json")).toBeDefined();
+		expect(loadKaryoFile(karyoFile)).toBeDefined();
 	});
 	
 });
@@ -14,8 +14,9 @@ describe("loadKaryoFile", function(){
 	var returnData;
 	var expectedChromosomes = {"Q_s6":{"genome_id":1,"rc":false,"length":416406,"seq":null},"Q_s8":{"length":257437,"genome_id":1,"rc":false,"seq":null},"R_gi_222106127_ref_NC_011988.1_":{"length":1283187,"rc":false,"genome_id":0,"seq":null},"Q_s2":{"seq":null,"length":531783,"rc":false,"genome_id":1},"Q_s22":{"length":117247,"genome_id":1,"rc":false,"seq":null}};
 	var expectedOrder = ["R_gi_222106127_ref_NC_011988.1_","Q_s6","Q_s8","Q_s22","Q_s2"];
+	
 	beforeEach(function(done){
-		loadKaryoFile(file, function(data){
+		loadKaryoFile(karyoFile, function(data){
 			console.log(data);
 			returnData = data;
 			done();
@@ -23,7 +24,7 @@ describe("loadKaryoFile", function(){
 	});
 	
 	it("the external karyo file should be load in and we have the chromosome object", function(){
-	expect(returnData.order).toEqual(expectedOrder);
+	expect(returnData.chromosomes).toEqual(expectedChromosomes);
 	});
 	
 	it("the external karyo file should be load in and we have the order array", function(){
