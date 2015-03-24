533834f1e7acbc2edc5c504783763b31c2125d42
Sonja Hohlfeld <Sonja-Hohlfeld@gmx.de>
2015-03-24 14:29:22 +0100

    erster Test für loadLink-File: schlug fehl, da noch nichts initialisiert wurde

--- wgaPipeline_jasmine/spec/testing_loadLinks.js
+++ wgaPipeline_jasmine/spec/testing_loadLinks.js
@@ -1,8 +1,8 @@
-var file = "data/link.json";
+//var file = "data/link.json";
 
 describe("test the verification of file", function(){
 	it("link file should be defined", function(){
-		
+		expect(file).toBeDefined;
 	});	
 });
 
--- wgaPipeline_jasmine/src/loadLinks.js
+++ wgaPipeline_jasmine/src/loadLinks.js
@@ -1,10 +1,2 @@
-function loadLinkFile(file, karyo, callback){
-	$.getJSON(file, function(data) {
-		if (typeof callback !== "undefined") {
-			callback(data);
-		}
-	});
-	return file;
-}
 
 
