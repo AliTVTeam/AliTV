ef18c5b10f9a76216022f32bea42101057705624
Sonja Hohlfeld <Sonja-Hohlfeld@gmx.de>
2015-03-24 14:33:09 +0100

    Neuer Test: es wird getestet ob das link-File innerhalb der Funktion loadLinkFile existiert, schlug fehl, da noch keine derartige Funktion existiert

--- wgaPipeline_jasmine/spec/testing_loadLinks.js
+++ wgaPipeline_jasmine/spec/testing_loadLinks.js
@@ -1,10 +1,11 @@
-//var file = "data/link.json";
-
 describe("test the verification of file", function(){
 	it("link file should be defined", function(){
 		var file = "data/link.json";
 		expect(file).toBeDefined();
 	});	
+	it("the link file in the function should be defined", function(){
+		expect(loadKaryoFile("data/link.json")).toBeDefined();
+	});
 });
 
 
