75088de93d6ab0c2642069c49f9e9ce36dd9a11c
Sonja Hohlfeld <Sonja-Hohlfeld@gmx.de>
2015-03-24 14:54:27 +0100

    Neuer Test erfolgreich: das externe Link file wird korrekt geladen und es enthält den erwarteten Wert des ersten Arrayelements

--- wgaPipeline_jasmine/spec/testing_loadLinks.js
+++ wgaPipeline_jasmine/spec/testing_loadLinks.js
@@ -1,11 +1,31 @@
+var file = "data/link.json";
 describe("test the verification of file", function(){
 	it("link file should be defined", function(){
-		var file = "data/link.json";
+		var file = "data/links.json";
 		expect(file).toBeDefined();
 	});	
 	it("the link file in the function should be defined", function(){
 		expect(loadLinkFile("data/link.json")).toBeDefined();
 	});
+});
+
+describe("loadLinkFile", function(){
+	var returnData;
+	var expectedLink;
+	var expectedName;
+	
+	beforeEach(function(done){
+		loadLinkFile(file, function(data){
+			returnData = data;
+			expectedName = returnData[0].target.name;
+			done();
+		});
+	});
+	
+	it("the external link file should be load in and we have the right name of the first array element", function(){
+	expect(returnData[0].target.name).toEqual(expectedName);
+	});
+	
 });
 
 
