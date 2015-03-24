17c934e075aec6e2a6567c79cb4e3a4afd1c5455
Sonja Hohlfeld <Sonja-Hohlfeld@gmx.de>
2015-03-24 10:50:45 +0100

    Problem Test 8 noch nicht gelöst, neue testfiles erstellt

--- wgaPipeline_jasmine/spec/testing_karyoToCoords.js
+++ wgaPipeline_jasmine/spec/testing_karyoToCoords.js
@@ -1,14 +1,15 @@
-var data = {chromosomes: {"Q_s6":{"genome_id":1,"rc":false,"length":416406,"seq":null},"Q_s8":{"length":257437,"genome_id":1,"rc":false,"seq":null},"R_gi_222106127_ref_NC_011988.1_":{"length":1283187,"rc":false,"genome_id":0,"seq":null},"Q_s2":{"seq":null,"length":531783,"rc":false,"genome_id":1},"Q_s22":{"length":117247,"genome_id":1,"rc":false,"seq":null}}, order:["R_gi_222106127_ref_NC_011988.1_","Q_s6","Q_s8","Q_s22","Q_s2"]}; 
 var dataChromosomes = {"Q_s6":{"genome_id":1,"rc":false,"length":416406,"seq":null},"Q_s8":{"length":257437,"genome_id":1,"rc":false,"seq":null},"R_gi_222106127_ref_NC_011988.1_":{"length":1283187,"rc":false,"genome_id":0,"seq":null},"Q_s2":{"seq":null,"length":531783,"rc":false,"genome_id":1},"Q_s22":{"length":117247,"genome_id":1,"rc":false,"seq":null}};
 var dataOrder = ["R_gi_222106127_ref_NC_011988.1_","Q_s6","Q_s8","Q_s22","Q_s2"];
 
 	describe("karyo_to_coords", function(){	
+		var data = {chromosomes: {"Q_s6":{"genome_id":1,"rc":false,"length":416406,"seq":null},"Q_s8":{"length":257437,"genome_id":1,"rc":false,"seq":null},"R_gi_222106127_ref_NC_011988.1_":{"length":1283187,"rc":false,"genome_id":0,"seq":null},"Q_s2":{"seq":null,"length":531783,"rc":false,"genome_id":1},"Q_s22":{"length":117247,"genome_id":1,"rc":false,"seq":null}}, order:["R_gi_222106127_ref_NC_011988.1_","Q_s6","Q_s8","Q_s22","Q_s2"]}; 
 		//testet, ob die Funktion wirklich aufgerufen wird und ob das richtige data übergeben wird
-		it("should test if the assigned data ist the returned data", function(){
-			expect(karyo_to_coords(data)).toEqual(data);
+		it("should test if data is defined", function(){
+			expect(karyo_to_coords(data)).toBeDefined();
 		});
 	
 	describe("set_spacer", function(){
+		var data = {chromosomes: {"Q_s6":{"genome_id":1,"rc":false,"length":416406,"seq":null},"Q_s8":{"length":257437,"genome_id":1,"rc":false,"seq":null},"R_gi_222106127_ref_NC_011988.1_":{"length":1283187,"rc":false,"genome_id":0,"seq":null},"Q_s2":{"seq":null,"length":531783,"rc":false,"genome_id":1},"Q_s22":{"length":117247,"genome_id":1,"rc":false,"seq":null}}, order:["R_gi_222106127_ref_NC_011988.1_","Q_s6","Q_s8","Q_s22","Q_s2"]}; 
 		var expectedSpacer = 10000;	
 		it("should call the function spacer, return a spacer and it should be less than to the expected spacer", function(){
 			expect(set_spacer(data)).toBeLessThan(expectedSpacer);
@@ -21,9 +22,13 @@ var dataOrder = ["R_gi_222106127_ref_NC_011988.1_","Q_s6","Q_s8","Q_s22","Q_s2"]
 	});
 	
 	describe("this part of karyo_to_coords add the drawing information to the data.chromosomes", function(){
+		var data = {chromosomes: {"Q_s6":{"genome_id":1,"rc":false,"length":416406,"seq":null},"Q_s8":{"length":257437,"genome_id":1,"rc":false,"seq":null},"R_gi_222106127_ref_NC_011988.1_":{"length":1283187,"rc":false,"genome_id":0,"seq":null},"Q_s2":{"seq":null,"length":531783,"rc":false,"genome_id":1},"Q_s22":{"length":117247,"genome_id":1,"rc":false,"seq":null}}, order:["R_gi_222106127_ref_NC_011988.1_","Q_s6","Q_s8","Q_s22","Q_s2"]}; 
+		var testData={chromosomes: {"Q_s6":{"genome_id":1,"rc":false,"length":416406,"seq":null, "name":"Q_s6"},"Q_s8":{"length":257437,"genome_id":1,"rc":false,"seq":null, "name":"Q_s8"},"R_gi_222106127_ref_NC_011988.1_":{"length":1283187,"rc":false,"genome_id":0,"seq":null, "name":"R_gi_222106127_ref_NC_011988.1_"},"Q_s2":{"seq":null,"length":531783,"rc":false,"genome_id":1,"name":"Q_s2"},"Q_s22":{"length":117247,"genome_id":1,"rc":false,"seq":null, "name":"Q_s22"}}, 
+						order:["R_gi_222106127_ref_NC_011988.1_","Q_s6","Q_s8","Q_s22","Q_s2"]}; 
+		
 		it("should expand the original object data.chromosomes which should differ from the new one", function(){
 			console.log(data);
-			expect(karyo_to_coords(data)).toEqual(data.chromosomes);
+			expect(karyo_to_coords(data)).toEqual(testData);
 		});
 	});
 	
--- wgaPipeline_jasmine/src/karyoToCoords.js
+++ wgaPipeline_jasmine/src/karyoToCoords.js
@@ -1,23 +1,38 @@
 function karyo_to_coords(data) {
+	var width=1200;
 	var total=[0,0];
 	var current=[0,0];
-
+	var index=0;
+	
 	var spacer = set_spacer(data);
 	$.each(data.chromosomes, function(key, value) {
+		console.log(value.genome_id);
 		total[value.genome_id] += value.length + spacer;
 	});
-	var index = 0;	
 	
-	data.chromosomes["Q_s6"].x = 1000;
+	for(var i=0;i<data.order.length;i++){
+		var key = data.order[i];
+		var value = data.chromosomes[key];
+		data.chromosomes[key] = {
+			"value" : value.length,
+			"index" : index++,
+			"x" : (current[value.genome_id] / total[value.genome_id]) * width,
+			"width" : (value.length / total[value.genome_id]) * width,
+			"genome_id" : value.genome_id,
+			"name" : key
+		};
+	};
 	
-	return data;
+	return data.chromosomes;
 }
 
 function set_spacer(data){
 	var spacer = 0;
 	$.each(data.chromosomes, function(key, value) {
+		console.log(value.length);
 		spacer += value.length;
 	});
-	spacer = spacer * 0.0038; // ca. 4% der Gesamtsumme aller Sequenzen								// entsprechen dem geeigneten Spacer
+	spacer = spacer * 0.0038; // ca. 4% der Gesamtsumme aller Sequenzen	// entsprechen dem geeigneten Spacer
+	console.log(spacer);
 	return spacer;
 }
