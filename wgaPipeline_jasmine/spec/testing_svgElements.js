7e2517e4b3acce8f3736f2e3a39916ef7c34e98e
Sonja Hohlfeld <Sonja-Hohlfeld@gmx.de>
2015-03-25 14:37:52 +0100

    Jetzt werden noch mehr svg-Elemente getestet, dafür muss erstmal ein neuer describe-Block angelegt werden. Wie erwartet schlug der Test fehl

--- wgaPipeline_jasmine/spec/testing_svgElements.js
+++ wgaPipeline_jasmine/spec/testing_svgElements.js
@@ -2,7 +2,9 @@ describe('Testing the first D3.js function drawKaryo', function() {
   var c;
 
   beforeEach(function() {
+	//var testData = karyo;
     c = drawKaryo();
+    //c.setData(testData);
     c.render();
   });
   
@@ -27,16 +29,29 @@ describe('Testing the first D3.js function drawKaryo', function() {
   function getSvg() {
     return d3.select('svg');
   }
-  
-  describe('testing the data' ,function() {	  
-	    it('should be null if no data has been specified', function() {
-	        expect(c.getData()).toBeNull();
-	    });
 
-	    it('karyo should be load in and it would able to update data with karyo', function() {
-	      c.setData(karyo);
-	      expect(c.getData()).toBe(karyo);
-	    });
-  	});
 
+describe('testing the data of drawKaryo' ,function() {	  
+	it('should be null if no data has been specified', function() {
+		expect(c.getData()).toBeNull();
+	});
+	it('karyo should be load in and it would able to update data with karyo', function() {
+		c.setData(karyo);
+		expect(c.getData()).toBe(karyo);
+	});
 });
+});
+
+describe('testing svg-Elements', function(){
+	beforeEach(function() {
+		var testData = karyo;
+	    c = drawKaryo();
+	    c.setData(testData);
+	    c.render();
+	  });
+	
+	it('new syntax: everything would be easier, but this test have to work: it should test if we have the correct data', function() {
+		c.setData(karyo);
+		expect(c.getData()).not.toBe(karyo);
+	});
+})
