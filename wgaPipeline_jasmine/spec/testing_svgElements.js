dfeca79c2eef312f2de880649c4a47dda02288d0
Sonja Hohlfeld <Sonja-Hohlfeld@gmx.de>
2015-03-25 13:53:53 +0100

    Neuer test für korrekte height und width, jetzt noch erfolglos, aber...

--- wgaPipeline_jasmine/spec/testing_svgElements.js
+++ wgaPipeline_jasmine/spec/testing_svgElements.js
@@ -15,13 +15,13 @@ describe('Testing the first D3.js function drawKaryo', function() {
         expect(getSvg()).not.toBeNull();
     });
 
-//    it('should have the correct height', function() {
-//      expect(getSvg().attr('width')).toBe('500');
-//    });
-//
-//    it('should have the correct width', function() {
-//      expect(getSvg().attr('width')).toBe('500');
-//    });
+    it('should have the correct height', function() {
+      expect(getSvg().attr('height')).not.toBe('3000');
+    });
+
+    it('should have the correct width', function() {
+      expect(getSvg().attr('width')).not.toBe('1200');
+    });
   });
 
   function getSvg() {
