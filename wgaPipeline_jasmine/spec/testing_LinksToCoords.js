2708c14a4259ba9ac99bf515307fbb44a09bd037
Sonja Hohlfeld <Sonja-Hohlfeld@gmx.de>
2015-03-25 10:48:24 +0100

    Alle bisherigen Tests funktionieren, auch die gesamte links_to_coords Funktion läuft soweit fehlerfrei

--- wgaPipeline_jasmine/spec/testing_LinksToCoords.js
+++ wgaPipeline_jasmine/spec/testing_LinksToCoords.js
@@ -1,28 +1,38 @@
-var karyo;
+var returnKaryo;
+var returnLinks;
 var links;
+var karyo;
 
 describe("link_to_coords", function(){
 	
 	beforeEach(function(done){
 		loadKaryoFile(karyoFile, function(data){
 			karyo_to_coords(data);
-			karyo=data;
+			returnKaryo=data.chromosomes;
 			
 			loadLinkFile(linkFile, function(data){
-			links=data;
+			returnLinks=data;
+			
+			links=0;
+			karyo=0;
 			done();
 			});
 		});
 	});
 	
+	
 	it("should test if links and karyo are defined when they are returned from their functions", function(){
-		expect(karyo).toBeDefined();
-		expect(links).toBeDefined();
+		expect(returnKaryo).toBeDefined();
+		expect(returnLinks).toBeDefined();
 	});
 	
-	it("should test if there is a function which get the defined karyo and links", function(){
+	it("should test if there is a function which get the defined karyo and links and which should create the ribbon information", function(){
+		links = returnLinks;
+		karyo = returnKaryo;
 		expect(link_to_coords(links, karyo)).toEqual(links);
+
 	})
+
 
 });	
 
