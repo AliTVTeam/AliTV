717f3b9c6ab18ae8916f90bd69d66dd48b8c5834
Sonja Hohlfeld <Sonja-Hohlfeld@gmx.de>
2015-03-25 09:39:47 +0100

    Test jetzt erfolgreich: karyo und links sind definiert

--- wgaPipeline_jasmine/spec/testing_LinksToCoords.js
+++ wgaPipeline_jasmine/spec/testing_LinksToCoords.js
@@ -16,8 +16,8 @@ describe("link_to_coords", function(){
 	});
 	
 	it("should test if links and karyo are defined when they are returned from their functions", function(){
-		expect(karyo).not.toBeDefined();
-		expect(links).not.toBeDefined();
+		expect(karyo).toBeDefined();
+		expect(links).toBeDefined();
 	});
 
 });	
