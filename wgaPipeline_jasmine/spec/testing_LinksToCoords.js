256619067cca49b817567edc306e42bcdc6659c9
Sonja Hohlfeld <Sonja-Hohlfeld@gmx.de>
2015-03-27 13:44:11 +0100

    Test für link_to_coors repariert, syntaxfehler behoben

--- wgaPipeline_jasmine/spec/testing_LinksToCoords.js
+++ wgaPipeline_jasmine/spec/testing_LinksToCoords.js
@@ -26,22 +26,22 @@ var karyo;
 //};
 
 //
-//describe("link_to_coords", function(){
-//	beforeEach(function(done){
-//		loadKaryoFile(karyoFile, function(data){
-//			karyo_to_coords(data);
-//			returnKaryo=data.chromosomes;
-//			
-//			loadLinkFile(linkFile, function(data){
-//			returnLinks=data;
-//			
-//			links=0;
-//			karyo=0;
-//			done();
-//			});
-//		});
-//
-//	});
+describe("link_to_coords", function(){
+	beforeEach(function(done){
+		loadKaryoFile(karyoFile, function(data){
+			karyo_to_coords(data);
+			returnKaryo=data.chromosomes;
+			
+			loadLinkFile(linkFile, function(data){
+			returnLinks=data;
+			
+			links=0;
+			karyo=0;
+			done();
+			});
+		});
+
+	});
 	it("should test if links and karyo are defined when they are returned from their functions", function(){
 		expect(returnKaryo).toBeDefined();
 		expect(returnLinks).toBeDefined();
