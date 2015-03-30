c69d49bd32344d54b2c08c8e6d38842f13c16e16
Markus Ankenbrand <markus@ankenbrand.me>
2015-03-30 15:29:01 +0200

    Added istanbul code coverage tool to grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -42,6 +42,17 @@ module.exports = function(grunt) {
 	    'wgaPipeline_jasmine/js/jquery.min.js',
 	    'wgaPipeline_jasmine/js/d3.v3.min.js'
 	  ],
+	  template: require('grunt-template-jasmine-istanbul'),
+          templateOptions: {
+            coverage: 'wgaPipeline_jasmine/coverage/coverage.json',
+            report: 'wgaPipeline_jasmine/coverage',
+            thresholds: {
+              lines: 75,
+              statements: 75,
+              branches: 75,
+              functions: 90
+            }
+          },
           //keepRunner : true
           //helpers: 'test/spec/*.js'
         }
@@ -53,7 +64,21 @@ module.exports = function(grunt) {
           jQuery: true
 	}
       },
-      all: ['Gruntfile.js', 'wgaPipeline_jasmine/src/*.js']
+      all: ['Gruntfile.js', 'wgaPipeline_jasmine/src/*.js', 'wgaPipeline_jasmine/js/wgaPipeline_circular.js']
+    },
+    coverage: {
+      default: {
+        options: {
+          thresholds: {
+            'statements': 90,
+            'branches': 90,
+            'lines': 90,
+            'functions': 90
+          },
+          dir: 'coverage',
+          root: 'wgaPipeline_jasmine'
+        }
+      }
     }
   });
 
@@ -61,6 +86,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-jasmine');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks("grunt-jsbeautifier");
+  grunt.loadNpmTasks('grunt-istanbul-coverage');
 
   // Default task(s).
   grunt.registerTask('default', ['jshint','jasmine']);
