9fee84a986231fc6590eb686a8c5d77dcd787960
Sonja Hohlfeld <Sonja-Hohlfeld@gmx.de>
2015-04-08 11:32:07 +0200

    Spion auf loadLinkFile korrekt gesetzt und liefert die erwarteten erfolgreichen Testergebnisse

--- d3/test/spec/testing_loadLinks.js
+++ d3/test/spec/testing_loadLinks.js
@@ -49,12 +49,12 @@ describe("Spy on loadLinkFile", function(){
 	});
 	
 	it("the spy tracks the arguments of loadLinkFile. In this case the arguments are either the filename or when you use grunt the url to the github-page. " +
-			"The test fails when you not assign the access to the data", function() {
+			"The test fails when you not assign the access to the data correctly.", function() {
 		if(navigator.userAgent.match(/phantomjs/i)){	
-			expect(loadLink.test).toHaveBeenCalledWith("https://raw.githubusercontent.com/BioInf-Wuerzburg/wgaPipeline/d3.js/d3_test/data/karyo.json");
+			expect(loadLink.test).toHaveBeenCalledWith("https://raw.githubusercontent.com/BioInf-Wuerzburg/wgaPipeline/d3.js/d3_test/data/link.json");
 		}
 		else{
-			expect(loadLink.test).toHaveBeenCalledWith("data/karyo.json");
+			expect(loadLink.test).toHaveBeenCalledWith("data/link.json");
 		}
 	    
 	});
