9e92c61c789e8b13ba83ef3fb274fa4ad7c1cd6c
Sonja Hohlfeld <Sonja-Hohlfeld@gmx.de>
2015-04-08 11:57:29 +0200

    Alte Tests aus getRibbon entfernt und einen spy gesetzt

--- d3/test/spec/testing_getRibbon.js
+++ d3/test/spec/testing_getRibbon.js
@@ -1,31 +1,53 @@
-//var returnKaryo;
-//var returnLinks;
-//var links;
-//var karyo;
-//
-//describe("getRibbon", function(){
-//	beforeEach(function(done){
-//		loadKaryoFile(karyoFile, function(data){
-//			karyo_to_coords(data);
-//			returnKaryo=data.chromosomes;
-//			
-//		loadLinkFile(linkFile, karyo, function(data){
-//			links = data;
-//			link_to_coords(links, karyo);
-//			
-//			done();
-//			});
+var returnKaryo;
+var returnLinks;
+var links;
+var fullkaryo;
+
+describe("describe function getRibbon", function(){
+	beforeEach(function(done){
+		loadKaryoFile(karyoFile, function(data){
+			karyo_to_coords(data);
+			returnKaryo=data.chromosomes;
+			
+		loadLinkFile(linkFile, returnKaryo, function(data){
+			links = data;
+			link_to_coords(links, returnKaryo);
+			
+			done();
+			});
+		});
+	});
+	
+	describe("Spy on getRibbon", function(){
+		//es wird ein test-Objekt loadKaryo angelegt, welches als test-Eigenschaft den Aufruf auf die Funktion loadKaryoFile enthält
+		var getRibbon = {};
+		getRibbon.test = function test(links){
+			getRibbon(links);
+		}
+		
+		//vor jedem spec wird der spy auf loadKaryo gesetzt
+		beforeEach(function() {
+			spyOn(getRibbon, 'test');
+		    getRibbon.test(links);
+		});
+		
+		
+		it("the spy tracks that the function loadKaryoFile was called", function() {
+		    expect(getRibbon.test).toHaveBeenCalled();
+		});
+		
+//		it("the spy tracks the arguments of loadKaryoFile. In this case the arguments are either the filename or when you use grunt the url to the github-page. " +
+//				"The test fails when you not assign the access to the data", function() {
+//			if(navigator.userAgent.match(/phantomjs/i)){	
+//				expect(loadKaryo.test).toHaveBeenCalledWith("https://raw.githubusercontent.com/BioInf-Wuerzburg/wgaPipeline/d3.js/d3_test/data/karyo.json");
+//			}
+//			else{
+//				expect(loadKaryo.test).toHaveBeenCalledWith("data/karyo.json");
+//			}
+//		    
 //		});
-//	});
-//	
-//	it("should test if there is a function getRibbon which return a true value", function(){
-//		expect(getRibbon(links)).toBeDefined();
-//	});
-//	
-//	it("should substract the targetline-information from the sourceline_information, the expected value should be 1", function(){
-//		expect(testLineDifference(links)).toBeDefined();
-//	})
-//
-//});	
-//
-//
+
+});
+});
+
+
