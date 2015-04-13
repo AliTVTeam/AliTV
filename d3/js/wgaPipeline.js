1de95867757eb88ea0200b59ca4db5db70fa4d36
Markus Ankenbrand <markus@ankenbrand.me>
2015-04-13 16:12:05 +0200

    Added documentation for wgaPipeline constructor

--- d3/js/wgaPipeline.js
+++ d3/js/wgaPipeline.js
@@ -1,6 +1,12 @@
 /**
+ * Creates an object of type wgaPipeline for drawing whole genome alignment visualizations
+ * @author Markus Ankenbrand <markus.ankenbrand@uni-wuerzburg.de> 
  * @constructor
- * @param{Object} svg - jQuery object containing a svg DOM element
+ * @param {Object} svg - jQuery object containing a svg DOM element. Visualizations will be drawn on this svg. Size may be changed by object methods. Previous content will be deleted.
+ * @example
+ * // initializes an wgaPipeline object (wga) on the svg element with id 'canvas'
+ * var svg = $('#canvas');
+ * var wga = new wgaPipeline(svg);
  */
 function wgaPipeline(svg) {
 	this.svg = svg;
