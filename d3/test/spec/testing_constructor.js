5dfe5b3ba7a7c5f9496021ce42cc8493a04f6549
Markus Ankenbrand <markus@ankenbrand.me>
2015-04-13 16:29:32 +0200

    Testing wgaPipeline constructor to set svg fails

--- d3/test/spec/testing_constructor.js
+++ d3/test/spec/testing_constructor.js
@@ -2,4 +2,12 @@ describe('The constructor is supposed to create a non empty object', function(){
 	it('Constructor wgaPipeline exists', function(){
 		expect(wgaPipeline).toBeDefined();
 	});
+	var svg = $('<svg></svg>');
+	var wga = new wgaPipeline(svg);
+	it('wgaPipelie object is not null', function(){
+		expect(wga).not.toBeNull();
+	});
+	it('the svg property is properly set', function(){
+		expect(wga.svg).toEqual(svg);
+	});
 });
