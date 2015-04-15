76f211a93cba95050f5fcbcdada228f9e3c42936
Markus Ankenbrand <markus@ankenbrand.me>
2015-04-15 10:49:11 +0200

    Test for functionality of setData method fails

--- d3/test/spec/test_wgaPipeline.js
+++ d3/test/spec/test_wgaPipeline.js
@@ -21,4 +21,23 @@ describe('The setData method of WgaPipeline objects is supposed to set the data'
 	it('setData method is supposed to be a function', function(){
 		expect(typeof wga.setData).toEqual('function');
 	});
+	var karyo = {
+		'order': ['c1', 'c2'],
+		'chromosomes': {
+		'c1': {'genome_id': 0, 'length': 2000, 'rc': false, 'seq': null},
+		'c2': {'genome_id': 1, 'length': 1000, 'rc': false, 'seq': null}
+		}
+	};
+	var features = {
+		'f1': {'karyo': 'c1', 'start': 300, 'end': 800},
+		'f2': {'karyo': 'c2', 'start': 100, 'end': 600}
+	};
+	var links = [
+		{'source': 'f1', 'target': 'f2', 'identity': 90}
+	];
+	var data = {'karyo': karyo, 'features': features, 'links': links};
+	wga.setData(data);
+	it('setData method is supposed to set the data variable', function(){
+		expect(wga.data).toEqual(data);
+	});
 });
