9f6c8bc76a1ef62872579e7c5910e18e722a4531
Markus Ankenbrand <markus@ankenbrand.me>
2015-04-15 10:46:26 +0200

    Added documentation for setData method

--- d3/js/wgaPipeline.js
+++ d3/js/wgaPipeline.js
@@ -13,6 +13,30 @@ function WgaPipeline(svg) {
 	this.data = {};
 }
 
+/**
+ * Sets the data of the WgaPipeline object.
+ * For the required format see the example
+ * @author Markus Ankenbrand <markus.ankenbrand@uni-wuerzburg.de>
+ * @param {Object} data - Object containing karyo, link and feature information
+ * @example
+ * var svg = $('#canvas');
+ * var wga = new wgaPipeline(svg);
+ * var karyo = {
+ * 	'order': ['c1', 'c2'],
+ * 	'chromosomes': {
+ * 	'c1': {'genome_id': 0, 'length': 2000, 'rc': false, 'seq': null},
+ * 	'c2': {'genome_id': 1, 'length': 1000, 'rc': false, 'seq': null}
+ * 	}
+ * };
+ * var features = {
+ * 	'f1': {'karyo': 'c1', 'start': 300, 'end': 800},
+ * 	'f2': {'karyo': 'c2', 'start': 100, 'end': 600}
+ * };
+ * var links = [
+ * 	{'source': 'f1', 'target': 'f2', 'identity': 90}
+ * ];
+ * wga.setData({'karyo': karyo, 'features': features, 'links': links};
+ */
 WgaPipeline.prototype.setData = function() {
 
 };
