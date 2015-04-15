0b5703167580044fa59c44f796d3d2e3ab82d6bd
Markus Ankenbrand <markus@ankenbrand.me>
2015-04-15 11:41:25 +0200

    Test for defined return value of getLinearKaryoCoords method fails

--- d3/test/spec/test_wgaPipeline.js
+++ d3/test/spec/test_wgaPipeline.js
@@ -18,27 +18,28 @@ describe('The constructor is supposed a proper WgaPipeline object', function(){
 	});
 });
 
+var karyo = {
+		'order': ['c1', 'c2'],
+		'chromosomes': {
+			'c1': {'genome_id': 0, 'length': 2000, 'rc': false, 'seq': null},
+			'c2': {'genome_id': 1, 'length': 1000, 'rc': false, 'seq': null}
+		}
+};
+var features = {
+		'f1': {'karyo': 'c1', 'start': 300, 'end': 800},
+		'f2': {'karyo': 'c2', 'start': 100, 'end': 600}
+};
+var links = [
+             {'source': 'f1', 'target': 'f2', 'identity': 90}
+             ];
+var data = {'karyo': karyo, 'features': features, 'links': links};
+
 describe('The setData method of WgaPipeline objects is supposed to set the data', function(){
 	var svg = $('<svg></svg>');
 	var wga = new WgaPipeline(svg);
 	it('setData method is supposed to be a function', function(){
 		expect(typeof wga.setData).toEqual('function');
 	});
-	var karyo = {
-		'order': ['c1', 'c2'],
-		'chromosomes': {
-		'c1': {'genome_id': 0, 'length': 2000, 'rc': false, 'seq': null},
-		'c2': {'genome_id': 1, 'length': 1000, 'rc': false, 'seq': null}
-		}
-	};
-	var features = {
-		'f1': {'karyo': 'c1', 'start': 300, 'end': 800},
-		'f2': {'karyo': 'c2', 'start': 100, 'end': 600}
-	};
-	var links = [
-		{'source': 'f1', 'target': 'f2', 'identity': 90}
-	];
-	var data = {'karyo': karyo, 'features': features, 'links': links};
 	wga.setData(data);
 	it('setData method is supposed to set the data variable', function(){
 		expect(wga.data).toEqual(data);
@@ -50,5 +51,10 @@ describe('The getLinearKaryoCoords method of WgaPipeline objects is supposed to 
 	var wga = new WgaPipeline(svg);
 	it('getLinearKaryoCoords method is supposed to be a function', function(){
 		expect(typeof wga.getLinearKaryoCoords).toEqual('function');
+	});
+	wga.setData(data);
+	var linearKaryoCoords = wga.getLinearKaryoCoords();
+	it('getLinearKaryoCoords method is supposed to return linearKaryoCoords', function(){
+		expect(linearKaryoCoords).toBeDefined();
 	});
 });
