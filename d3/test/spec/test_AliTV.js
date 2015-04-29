1881492a92927de8688e7e621cd2b5fbc34e93fd
Sonja Hohlfeld <Sonja-Hohlfeld@gmx.de>
2015-04-29 08:50:42 +0200

    Complete next test case: there should be three chromosomes on three different genomes and two links, but only one should be drawn because the other is not adjacent

--- d3/test/spec/test_AliTV.js
+++ d3/test/spec/test_AliTV.js
@@ -700,15 +700,17 @@ describe('The drawLinearLinks method of AliTV objects is supposed to draw links 
 		expect(ali.svgD3.selectAll('.karyo').size()).toEqual(3);
 		expect(ali.svgD3.selectAll('.link').size()).toEqual(2);
 	});
-//	it('there should be exactly three karyos ad two links in the simple test svg (actual there are exactly three links, but only two are drawn because the third one is not an adjacent link)', function(){
-//		var linearKaryoCoords = ali.getLinearKaryoCoords();
-//		var linearLinkCoords = ali.getLinearLinkCoords(linearKaryoCoords);
-//		ali.drawLinearKaryo(linearKaryoCoords);
-//		ali.drawLinearLinks(linearLinkCoords);
-//		expect(linearLinkCoords.length).toEqual(3);
-//		expect(ali.svgD3.selectAll('.karyo').size()).toEqual(2);
-//		expect(ali.svgD3.selectAll('.link').size()).toEqual(2);
-//	});
+	it('there should be exactly three karyos ad two links in the simple test svg (actual there are exactly three links, but only two are drawn because the third one is not an adjacent link)', function(){
+		ali.setData({karyo:karyo5,features:features3, links:links5});
+		ali.setFilters(filters5);
+		var linearKaryoCoords = ali.getLinearKaryoCoords();
+		var linearLinkCoords = ali.getLinearLinkCoords(linearKaryoCoords);
+		ali.drawLinearKaryo(linearKaryoCoords);
+		ali.drawLinearLinks(linearLinkCoords);
+		expect(linearLinkCoords.length).toEqual(3);
+		expect(ali.svgD3.selectAll('.karyo').size()).toEqual(3);
+		expect(ali.svgD3.selectAll('.link').size()).toEqual(1);
+	});
 //	it('the drawn karyos have the expected height', function(){
 //		linearKaryoCoords = wga.getLinearKaryoCoords();
 //		wga.drawLinearKaryo(linearKaryoCoords);
