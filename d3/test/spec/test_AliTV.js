902e239774ee4e83571cc3abb854b82f410fadca
Sonja Hohlfeld <Sonja-Hohlfeld@gmx.de>
2015-04-29 08:43:03 +0200

    Create new filters and add them to the test data

--- d3/test/spec/test_AliTV.js
+++ d3/test/spec/test_AliTV.js
@@ -124,6 +124,16 @@ var filters4 = {'karyo': {
 		}
 	}
 };
+var filters5 = {'karyo': {
+	'order': ['c1', 'c2', 'c4'],
+	'genome_order': [0, 1, 2],
+	'chromosomes': {
+		'c1': {'reverse': false, 'visible': null},
+		'c2': {'reverse': false, 'visible': null},
+		'c4': {'reverse': false, 'visible': null}
+	}
+}
+};
 var filters4_reverse = {'karyo': {
 	'order': ['c1', 'c2', 'c3', 'c4'],
 	'genome_order': [0, 1, 2],
@@ -679,13 +689,15 @@ describe('The drawLinearLinks method of AliTV objects is supposed to draw links 
 		expect(ali.svgD3.selectAll('.link').size()).toEqual(1);
 	});
 	it('there should be exactly two links and three chromosomes in the simple test svg', function(){
-//		ali.setData({karyo:karyo4,features:features2, links:links2});
-//		ali.setFilters(filters4);
+		ali.setData({karyo:karyo5,features:features3, links:links5});
+		ali.setFilters(filters4);
 		var linearKaryoCoords = ali.getLinearKaryoCoords();
 		var linearLinkCoords = ali.getLinearLinkCoords(linearKaryoCoords);
+		console.log(linearKaryoCoords);
+		console.log(linearLinkCoords);
 		ali.drawLinearKaryo(linearKaryoCoords);
 		ali.drawLinearLinks(linearLinkCoords);
-		expect(ali.svgD3.selectAll('.karyo').size()).toEqual(2);
+		expect(ali.svgD3.selectAll('.karyo').size()).toEqual(3);
 		expect(ali.svgD3.selectAll('.link').size()).toEqual(2);
 	});
 //	it('there should be exactly three karyos ad two links in the simple test svg (actual there are exactly three links, but only two are drawn because the third one is not an adjacent link)', function(){
