1a8858eab3075ee5930803f70b998f6a84611f4d
Sonja Hohlfeld <Sonja-Hohlfeld@gmx.de>
2015-04-29 09:25:59 +0200

    Comment last test out because i have made a logical mistake

--- d3/test/spec/test_AliTV.js
+++ d3/test/spec/test_AliTV.js
@@ -708,14 +708,14 @@ describe('The drawLinearLinks method of AliTV objects is supposed to draw links 
 		expect(ali.svgD3.selectAll('.karyo').size()).toEqual(3);
 		expect(ali.svgD3.selectAll('.link').size()).toEqual(2);
 	});
-	it('there should be exactly three karyos ad one links in the simple test svg (actual there are exactly two links, but only one is drawn because the second one is not an adjacent link)', function(){
-		ali.setData({karyo:karyo5,features:features4, links:links6});
-		ali.setFilters(filters5);
-		var linearKaryoCoords = ali.getLinearKaryoCoords();
-		var linearLinkCoords = ali.getLinearLinkCoords(linearKaryoCoords);
-		ali.drawLinearKaryo(linearKaryoCoords);
-		ali.drawLinearLinks(linearLinkCoords);
-		expect(ali.svgD3.selectAll('.karyo').size()).toEqual(3);
-		expect(ali.svgD3.selectAll('.link').size()).toEqual(1);
-	});
+//	it('there should be exactly three karyos ad one links in the simple test svg (actual there are exactly two links, but only one is drawn because the second one is not an adjacent link)', function(){
+//		ali.setData({karyo:karyo5,features:features4, links:links6});
+//		ali.setFilters(filters5);
+//		var linearKaryoCoords = ali.getLinearKaryoCoords();
+//		var linearLinkCoords = ali.getLinearLinkCoords(linearKaryoCoords);
+//		ali.drawLinearKaryo(linearKaryoCoords);
+//		ali.drawLinearLinks(linearLinkCoords);
+//		expect(ali.svgD3.selectAll('.karyo').size()).toEqual(3);
+//		expect(ali.svgD3.selectAll('.link').size()).toEqual(1);
+//	});
 });
