8845be40e0d4e2d2a9189de53472dd08ea7f5084
Sonja Hohlfeld <Sonja-Hohlfeld@gmx.de>
2015-04-29 09:40:20 +0200

    Make the test pass: the function drawLinear draws links and karyos

--- d3/test/spec/test_AliTV.js
+++ d3/test/spec/test_AliTV.js
@@ -332,7 +332,7 @@ describe('The drawLinear method of AliTV objects is supposed to draw the linear 
 	});
 	it('there should be exactly three karyos and one link in the test svg', function(){
 		wga.drawLinear();
-		expect(wga.svgD3.selectAll('.link').size()).toEqual(2);
+		expect(wga.svgD3.selectAll('.link').size()).toEqual(1);
 		expect(wga.svgD3.selectAll('.karyo').size()).toEqual(3);
 	});
 	it('the drawn karyos have the expected height', function(){
