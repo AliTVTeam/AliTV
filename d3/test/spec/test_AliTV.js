8ec08b87ebb8a81a89da46f1ad00ff96329d72bd
Sonja Hohlfeld <Sonja-Hohlfeld@gmx.de>
2015-04-30 11:47:59 +0200

    Write another test for checking the right color and it fails

--- d3/test/spec/test_AliTV.js
+++ d3/test/spec/test_AliTV.js
@@ -764,8 +764,10 @@ describe('The colorLinkByIdentity method of AliTV objects is supposed to color l
 		expect(typeof ali.colorLinksByIdentity).toEqual('function');
 	});
 	it('the colorLinksByIdentity method is supposed to return the color "#d21414" because the function get an identity of 0', function(){
-		expect(ali.colorLinksByIdentity(0)).toEqual("#d21414");
-		
+		expect(ali.colorLinksByIdentity(0)).toEqual("#d21414");		
+	});
+	it('the colorLinksByIdentity method is supposed to return the color "#1DAD0A" because the function get an identity of 100', function(){
+		expect(ali.colorLinksByIdentity(100)).toEqual("#d21414");		
 	});
 });
 
