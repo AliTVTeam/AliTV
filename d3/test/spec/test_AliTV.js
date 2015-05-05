4d198d1cd7407f5700f0c9606bd39353033476b0
Sonja Hohlfeld <Sonja-Hohlfeld@gmx.de>
2015-05-05 16:27:16 +0200

    Test for getLinearTickCoords fails

--- d3/test/spec/test_AliTV.js
+++ d3/test/spec/test_AliTV.js
@@ -827,8 +827,8 @@ describe('The getLinearTickCoords method is supposed to calculate coords for the
 		ali.setData({karyo:karyo6});
 		ali.setFilters(filters6);
 		var linearKaryoCoords = ali.getLinearKaryoCoords();
-		var ticks = ali.getLinearTickCoords;
-		var expectedTicks = [0, 50, 100, 150, 200, 250, 300, 350, 400, 450, 500, 550, 600, 650, 700, 750, 800, 850, 900, 950, 1000];
+		var ticks = ali.getLinearTickCoords(linearKaryoCoords);
+		var expectedTicks = [ 50, 100, 150, 200, 250, 300, 350, 400, 450, 500, 550, 600, 650, 700, 750, 800, 850, 900, 950, 1000];
 		
 		expect(ticks).toEqual(expectedTicks);		
 	});
