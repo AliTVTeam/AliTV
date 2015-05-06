0eccd48c75e6b8587a0a653bafce262b3ebc4422
Sonja Hohlfeld <Sonja-Hohlfeld@gmx.de>
2015-05-06 17:00:34 +0200

    Add helper function to trigger mouse events

--- d3/test/lib/helper.js
+++ d3/test/lib/helper.js
@@ -1,10 +1,10 @@
 // Perform D3 click events with jquery
 // http://stackoverflow.com/a/11180172
 
-jQuery.fn.d3Click = function() {
+jQuery.fn.d3Trigger = function(type) {
 	this.each(function(i, e) {
 		var evt = document.createEvent("MouseEvents");
-		evt.initMouseEvent("click", true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+		evt.initMouseEvent(type, true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
 
 		e.dispatchEvent(evt);
 	});
