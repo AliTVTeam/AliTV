eb1e24b52a028c3ebccd2a33cd8c5c520107d7d9
Markus Ankenbrand <markus@ankenbrand.me>
2015-05-08 16:20:15 +0200

    added test for reverse information usage in drawCircularKaryo

--- d3/test/spec/test_AliTV.js
+++ d3/test/spec/test_AliTV.js
@@ -27,6 +27,86 @@ var defaultConf =  {
 		midLinkIdentityColor: "#FFEE05"
 };
 
+var customMatchers = {
+		toHaveSameAngles: function(util, customEqualityTesters) {
+    		return { 
+    			compare: function(actual, expected){
+    				var result = {pass: true};
+    				if(actual.length !== expected.length){
+    					result.pass = false;
+    				} else {
+    					var precision = 8;
+    					var factor = Math.pow(10, precision);
+    					for(var i=0; i<actual.length; i++){
+    						var startActual = Math.round(actual[i].startAngle*factor)/factor;
+    						var startExpected = Math.round(expected[i].startAngle*factor)/factor;
+    						var endActual = Math.round(actual[i].endAngle*factor)/factor;
+    						var endExpected = Math.round(expected[i].endAngle*factor)/factor;
+    						if((startActual !== startExpected) || (endActual !== endExpected)){
+    							result.pass = false;
+    						}
+    					}
+    				}
+    				return result;
+    			}
+    		};
+    	},
+    	toHaveSameCoordinates: function(util, customEqualityTesters) {
+    		return { 
+    			compare: function(actual, expected){
+    				var compare = function(a,b){
+    					return (a < b) ? -1 : 1;
+    				};
+    				actual.sort(compare);
+    				expected.sort(compare);
+    				var result = {pass: true};
+    				if(actual.length !== expected.length){
+    					result.pass = false;
+    					result.message = "arrays do not have the same number of objects";
+    				} else {
+    					var precision = 8;
+    					var factor = Math.pow(10, precision);
+    					for(var i=0; i<actual.length; i++){
+    						if(actual[i].linkID !== expected[i].linkID){
+    							result.pass = false;
+    							result.message = "mismatch in linkID: " + actual[i].linkID + " vs " + expected[i].linkID;
+    						}
+    						var source0Actual = {x :Math.round(actual[i].source0.x*factor)/factor, y: Math.round(actual[i].source0.y*factor)/factor};
+    						var source0Expected = {x :Math.round(expected[i].source0.x*factor)/factor, y: Math.round(expected[i].source0.y*factor)/factor};
+    						if((source0Actual.x !== source0Expected.x) || (source0Actual.y !== source0Expected.y)){
+    							result.pass = false;
+    							result.message = "mismatch in source0 of " + actual[i].linkID + ": (" +source0Actual.x +", "+ source0Actual.y +") vs ("+source0Expected.x +", "+  source0Expected.y+")";
+    						}
+    						var target0Actual = {x :Math.round(actual[i].target0.x*factor)/factor, y: Math.round(actual[i].target0.y*factor)/factor};
+    						var target0Expected = {x :Math.round(expected[i].target0.x*factor)/factor, y: Math.round(expected[i].target0.y*factor)/factor};
+    						if((target0Actual.x !== target0Expected.x) || (target0Actual.y !== target0Expected.y)){
+    							result.pass = false;
+    							result.message = "mismatch in target0 of " + actual[i].linkID + ": (" +target0Actual.x +", "+ target0Actual.y +") vs ("+target0Expected.x +", "+  target0Expected.y+")";
+    						}
+    						var source1Actual = {x :Math.round(actual[i].source1.x*factor)/factor, y: Math.round(actual[i].source1.y*factor)/factor};
+    						var source1Expected = {x :Math.round(expected[i].source1.x*factor)/factor, y: Math.round(expected[i].source1.y*factor)/factor};
+    						if((source1Actual.x !== source1Expected.x) || (source1Actual.y !== source1Expected.y)){
+    							result.pass = false;
+    							result.message = "mismatch in source1 of " + actual[i].linkID + ": (" +source1Actual.x +", "+ source1Actual.y +") vs ("+source1Expected.x +", "+  source1Expected.y+")";
+    						}
+    						var target1Actual = {x :Math.round(actual[i].target1.x*factor)/factor, y: Math.round(actual[i].target1.y*factor)/factor};
+    						var target1Expected = {x :Math.round(expected[i].target1.x*factor)/factor, y: Math.round(expected[i].target1.y*factor)/factor};
+    						if((target1Actual.x !== target1Expected.x) || (target1Actual.y !== target1Expected.y)){
+    							result.pass = false;
+    							result.message = "mismatch in target1 of " + actual[i].linkID + ": (" +target1Actual.x +", "+ target1Actual.y +") vs ("+target1Expected.x +", "+  target1Expected.y+")";
+    						}
+    						if(actual[i].adjacent !== expected[i].adjacent){
+    							result.pass = false;
+    							result.message = "wrong adjacency: " + actual[i].adjancy + " vs " + expected[i].adjacency;
+    						}
+    					}
+    				}
+    				return result;
+    			}
+    		};
+    	}
+    }
+
 describe('The constructor is supposed a proper AliTV object', function(){
 	it('Constructor AliTV exists', function(){
 		expect(AliTV).toBeDefined();
@@ -391,31 +471,7 @@ describe('The drawLinear method of AliTV objects is supposed to draw the linear 
 
 describe('The getCircularKaryoCoords method of AliTV objects is supposed to calculate coordinates for the karyos in the circular case', function(){
 	beforeEach(function() {
-	    jasmine.addMatchers({
-	    	toHaveSameAngles: function(util, customEqualityTesters) {
-	    		return { 
-	    			compare: function(actual, expected){
-	    				var result = {pass: true};
-	    				if(actual.length !== expected.length){
-	    					result.pass = false;
-	    				} else {
-	    					var precision = 8;
-	    					var factor = Math.pow(10, precision);
-	    					for(var i=0; i<actual.length; i++){
-	    						var startActual = Math.round(actual[i].startAngle*factor)/factor;
-	    						var startExpected = Math.round(expected[i].startAngle*factor)/factor;
-	    						var endActual = Math.round(actual[i].endAngle*factor)/factor;
-	    						var endExpected = Math.round(expected[i].endAngle*factor)/factor;
-	    						if((startActual !== startExpected) || (endActual !== endExpected)){
-	    							result.pass = false;
-	    						}
-	    					}
-	    				}
-	    				return result;
-	    			}
-	    		};
-	    	}
-	    });
+	    jasmine.addMatchers(customMatchers);
 	});
 	var svg = $('<svg></svg>');
 	var wga = new AliTV(svg);
@@ -644,62 +700,7 @@ describe('The drawCircular method of AliTV objects is supposed to draw the circu
 
 describe('The getLinearLinkCoords method of AliTV objects is supposed to calculate coordinates for the links in the linear case and check if links are adjacent or not', function(){
 	beforeEach(function() {
-	    jasmine.addMatchers({
-	    	toHaveSameCoordinates: function(util, customEqualityTesters) {
-	    		return { 
-	    			compare: function(actual, expected){
-	    				var compare = function(a,b){
-	    					return (a < b) ? -1 : 1;
-	    				};
-	    				actual.sort(compare);
-	    				expected.sort(compare);
-	    				var result = {pass: true};
-	    				if(actual.length !== expected.length){
-	    					result.pass = false;
-	    					result.message = "arrays do not have the same number of objects";
-	    				} else {
-	    					var precision = 8;
-	    					var factor = Math.pow(10, precision);
-	    					for(var i=0; i<actual.length; i++){
-	    						if(actual[i].linkID !== expected[i].linkID){
-	    							result.pass = false;
-	    							result.message = "mismatch in linkID: " + actual[i].linkID + " vs " + expected[i].linkID;
-	    						}
-	    						var source0Actual = {x :Math.round(actual[i].source0.x*factor)/factor, y: Math.round(actual[i].source0.y*factor)/factor};
-	    						var source0Expected = {x :Math.round(expected[i].source0.x*factor)/factor, y: Math.round(expected[i].source0.y*factor)/factor};
-	    						if((source0Actual.x !== source0Expected.x) || (source0Actual.y !== source0Expected.y)){
-	    							result.pass = false;
-	    							result.message = "mismatch in source0 of " + actual[i].linkID + ": (" +source0Actual.x +", "+ source0Actual.y +") vs ("+source0Expected.x +", "+  source0Expected.y+")";
-	    						}
-	    						var target0Actual = {x :Math.round(actual[i].target0.x*factor)/factor, y: Math.round(actual[i].target0.y*factor)/factor};
-	    						var target0Expected = {x :Math.round(expected[i].target0.x*factor)/factor, y: Math.round(expected[i].target0.y*factor)/factor};
-	    						if((target0Actual.x !== target0Expected.x) || (target0Actual.y !== target0Expected.y)){
-	    							result.pass = false;
-	    							result.message = "mismatch in target0 of " + actual[i].linkID + ": (" +target0Actual.x +", "+ target0Actual.y +") vs ("+target0Expected.x +", "+  target0Expected.y+")";
-	    						}
-	    						var source1Actual = {x :Math.round(actual[i].source1.x*factor)/factor, y: Math.round(actual[i].source1.y*factor)/factor};
-	    						var source1Expected = {x :Math.round(expected[i].source1.x*factor)/factor, y: Math.round(expected[i].source1.y*factor)/factor};
-	    						if((source1Actual.x !== source1Expected.x) || (source1Actual.y !== source1Expected.y)){
-	    							result.pass = false;
-	    							result.message = "mismatch in source1 of " + actual[i].linkID + ": (" +source1Actual.x +", "+ source1Actual.y +") vs ("+source1Expected.x +", "+  source1Expected.y+")";
-	    						}
-	    						var target1Actual = {x :Math.round(actual[i].target1.x*factor)/factor, y: Math.round(actual[i].target1.y*factor)/factor};
-	    						var target1Expected = {x :Math.round(expected[i].target1.x*factor)/factor, y: Math.round(expected[i].target1.y*factor)/factor};
-	    						if((target1Actual.x !== target1Expected.x) || (target1Actual.y !== target1Expected.y)){
-	    							result.pass = false;
-	    							result.message = "mismatch in target1 of " + actual[i].linkID + ": (" +target1Actual.x +", "+ target1Actual.y +") vs ("+target1Expected.x +", "+  target1Expected.y+")";
-	    						}
-	    						if(actual[i].adjacent !== expected[i].adjacent){
-	    							result.pass = false;
-	    							result.message = "wrong adjacency: " + actual[i].adjancy + " vs " + expected[i].adjacency;
-	    						}
-	    					}
-	    				}
-	    				return result;
-	    			}
-	    		};
-	    	}
-	    });
+	    jasmine.addMatchers(customMatchers);
 	});
 	var svg = $('<svg></svg>');
 	var ali = new AliTV(svg);
@@ -1177,10 +1178,13 @@ describe('The drawLinearTicks method is supposed to draw ticks in the linear lay
 });
 
 describe('A left mouseclick on a chromosome should change the reverse information of this chromosome', function(){
-	var svg = $('<svg></svg>');
-	var ali = new AliTV(svg);
-	ali.setData(data);
-	ali.setFilters(filters);
+    var svg = $('<svg></svg>');
+    var ali = new AliTV(svg);
+	beforeEach(function() {
+	    jasmine.addMatchers(customMatchers);
+	    ali.setData(data);
+	    ali.setFilters(filters);
+	});
 	
 	it('if the current reverse information is false after the click event it should be true', function(done){	
 		ali.filters.karyo.chromosomes.c1.reverse = false;
@@ -1241,6 +1245,23 @@ describe('A left mouseclick on a chromosome should change the reverse informatio
 		
 		setTimeout(function(){
 			expect(expectedLinks).toEqual(linearLinkCoords);
+			done();
+		}, 1000);
+	});
+	it('the click event should also work in the circular layout', function(done){
+		ali.filters.karyo.chromosomes.c1.reverse = true;
+		ali.drawCircular();
+		ali.svg.find('.karyo').eq(0).d3Trigger("click");
+		var circularKaryoCoords = ali.getCircularKaryoCoords();
+		var expAnglePerBase = 2*Math.PI/(3000+2*defaultConf.circular.karyoDistance);
+		var expAnglePerSpace = expAnglePerBase * defaultConf.circular.karyoDistance;
+		var expectedCoords = [
+            {'karyo': 'c1', 'endAngle': 0, 'startAngle': 2000*expAnglePerBase},
+            {'karyo': 'c2', 'startAngle': 2000*expAnglePerBase + expAnglePerSpace, 'endAngle': 3000*expAnglePerBase + expAnglePerSpace}
+        ];
+		
+		setTimeout(function(){
+			expect(circularKaryoCoords).toHaveSameAngles(expectedCoords);
 			done();
 		}, 1000);
 	});
