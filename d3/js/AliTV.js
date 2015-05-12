303368d0c15a73790f00c28835c240fa11992323
Sonja Hohlfeld <Sonja-Hohlfeld@gmx.de>
2015-05-12 11:42:45 +0200

    Add new function setCanvasSize, write docu for it and make the test pass

--- d3/js/AliTV.js
+++ d3/js/AliTV.js
@@ -835,3 +835,19 @@ AliTV.prototype.getCanvasSize = function() {
 	size.height = this.conf.height;
 	return size;
 };
+
+/**
+ * This function replaces the old size of the drawing area with the new size in the config-object.
+ * It is called by a blur()-event, when the decription field loses focus.
+ * When the method gets a wrong value it throws an error message.
+ * @param {Object} The function gets the size of the svg drawing area which can be set by the user. Its structure is shown in the following example:
+ * @example size = {width: 1000, height: 1000}
+ * @throws Will throw an error if the argument is empty.
+ * @throws Will throw an error if the argument is not a number.
+ * @throws Will throw an error if the argument is less than 0 or equal to 0.
+ * @author Sonja Hohlfeld
+ */
+
+AliTV.prototype.setCanvasSize = function(size) {
+
+};
