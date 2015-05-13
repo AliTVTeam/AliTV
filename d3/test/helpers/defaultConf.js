a98b5dd99573cf3cc2a1bb11f91f03d08387880b
Markus Ankenbrand <markus@ankenbrand.me>
2015-05-13 17:54:35 +0200

    moved defaultConf to separate helper file - tests failing

---
+++ d3/test/helpers/defaultConf.js
@@ -0,0 +1,27 @@
+var defaultConf =  {
+		linear: {
+			genomeDistance: 300,
+			drawAllLinks: false,
+			startLineColor: "#49006a",
+			endLineColor: "#1d91c0",
+		},
+		circular: {
+			outerRadius: 450,
+			tickSize: 5
+		},
+		graphicalParameters: {
+			width: 1000,
+			height: 1000,
+			karyoHeight: 30,
+			karyoDistance: 10,
+			linkKaryoDistance: 10,
+			tickDistance: 100
+		},
+		minLinkIdentity: 40,
+		maxLinkIdentity: 100,
+		midLinkIdentity: 60,
+		minLinkIdentityColor: "#D21414",
+		maxLinkIdentityColor: "#1DAD0A",
+		midLinkIdentityColor: "#FFEE05",
+		layout: "linear"
+};
