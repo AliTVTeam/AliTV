628ac9defc589c79ea136b5196cd4cb7a95150b2
Sonja Hohlfeld <Sonja-Hohlfeld@gmx.de>
2015-05-21 13:19:33 +0200

    Add new filter: skipChromosomesWithoutLinkageInforamtion in order to skip chromosomes or not, when they have no linkage information. Add this new value to the filters.json and write the docu for it

--- d3/js/AliTV.js
+++ d3/js/AliTV.js
@@ -40,18 +40,19 @@ function AliTV(svg) {
 	this.data = {};
 	/**
 	 * property to store data specific drawing options (structure highly dependent on data structure)
-	 * @property {Object}  filters                      				- the data dependent displaying information
-	 * @property {Object}  filters.karyo                        		- the chromosome dependent displaying information
-	 * @property {Boolean} filters.skipChromosomesWithoutVisibleLinks	- If a chromosome has no visible links, because they are filtered, the user have the possibility to skip this chromosome.
-	 * @property {Array}   filters.karyo.order                  		- array of chromosome IDs in the desired order (circular layout)
-	 * @property {Array}   filters.karyo.genome_order          			- array of genome IDs in the desired order (linear layout)
-	 * @property {Object}  filters.karyo.chromosomes           			- the chromosome drawing details, karyo IDs as keys
-	 * @property {Boolean} filters.karyo.chromosomes.reverse    		- should the sequence be treated as its reverse (complement)
-	 * @property {Boolean} filters.karyo.chromosomes.visible    		- should the sequence be displayed at all
-	 * @property {Number}  filters.links.minLinkIdentity	    		- The minimum identity of links which should be draw.
-	 * @property {Number}  filters.links.maxLinkIdentity    			- The maximum identity of links which should be draw.
-	 * @property {Number}  filters.links.minLinkLength  				- The minimum length of links, which should be draw in bp.
-	 * @property {Number}  filters.links.maxLinkLength  				- The maximum length of links, which should be draw in bp.
+	 * @property {Object}  filters                      					- the data dependent displaying information
+	 * @property {Object}  filters.karyo                        			- the chromosome dependent displaying information
+	 * @property {Boolean} filters.skipChromosomesWithoutVisibleLinks		- If a chromosome has no visible links, because they are filtered, the user have the possibility to skip this chromosome.
+	 * @property {Boolean} filters.skipChromosomesWithoutLinkageInformation - If a chromosome has no linkage information, the user have the possibility to skip these chromosomes or not.
+	 * @property {Array}   filters.karyo.order                  			- array of chromosome IDs in the desired order (circular layout)
+	 * @property {Array}   filters.karyo.genome_order          				- array of genome IDs in the desired order (linear layout)
+	 * @property {Object}  filters.karyo.chromosomes           				- the chromosome drawing details, karyo IDs as keys
+	 * @property {Boolean} filters.karyo.chromosomes.reverse    			- should the sequence be treated as its reverse (complement)
+	 * @property {Boolean} filters.karyo.chromosomes.visible    			- should the sequence be displayed at all
+	 * @property {Number}  filters.links.minLinkIdentity	    			- The minimum identity of links which should be draw.
+	 * @property {Number}  filters.links.maxLinkIdentity    				- The maximum identity of links which should be draw.
+	 * @property {Number}  filters.links.minLinkLength  					- The minimum length of links, which should be draw in bp.
+	 * @property {Number}  filters.links.maxLinkLength  					- The maximum length of links, which should be draw in bp.
 	 */
 	this.filters = {};
 	/**
