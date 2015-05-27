0bb218c9c45f78368d31280b008c6be8d2d78c24
Sonja Hohlfeld <Sonja-Hohlfeld@gmx.de>
2015-05-27 09:16:04 +0200

    Add more test data

--- d3/test/data/testData.js
+++ d3/test/data/testData.js
@@ -89,6 +89,16 @@ var karyo8 = {
 			'c7': {'genome_id': 2, 'length': 1000, 'seq': null}
 		}
 };
+var karyo9 = {
+		'chromosomes': {
+			'c1': {'genome_id': 0, 'length': 1000, 'seq': null},
+			'c2': {'genome_id': 0, 'length': 1000, 'seq': null},
+			'c3': {'genome_id': 1, 'length': 1000, 'seq': null},
+			'c4': {'genome_id': 1, 'length': 1000, 'seq': null},
+			'c5': {'genome_id': 2, 'length': 500, 'seq': null},
+			'c6': {'genome_id': 2, 'length': 250, 'seq': null}
+		}
+};
 var filters4 = {'karyo': {
 		'order': ['c1', 'c2', 'c3', 'c4'],
 		'genome_order': [0, 1, 2],
@@ -222,6 +232,21 @@ var filters14 = {'karyo': {
 	"links": {"minLinkIdentity": 0, "maxLinkIdentity": 100, "minLinkLength": 100, "maxLinkLength": 10000}
 }
 
+var filters15 = {'karyo': {
+	'order': ['c1', 'c2', 'c3', 'c4', 'c5', 'c6', 'c7'],
+	'genome_order': [0, 1, 2],
+	'chromosomes': {
+		'c1': {'reverse': false, 'visible': true},
+		'c2': {'reverse': false, 'visible': true},
+		'c3': {'reverse': false, 'visible': true},
+		'c4': {'reverse': false, 'visible': true},
+		'c5': {'reverse': false, 'visible': true},
+		'c6': {'reverse': false, 'visible': true},
+		'c7': {'reverse': false, 'visible': true}
+	}},
+	"links": {"minLinkIdentity": 0, "maxLinkIdentity": 100, "minLinkLength": 100, "maxLinkLength": 10000}
+}
+
 var features = {
 		'f1': {'karyo': 'c1', 'start': 300, 'end': 800},
 		'f2': {'karyo': 'c2', 'start': 100, 'end': 600}
@@ -285,6 +310,18 @@ var features9 = {
 		'f4': {'karyo': 'c3', 'start': 600, 'end': 800},
 		'f5': {'karyo': 'c4', 'start': 600, 'end': 800}
 };
+var features10 = {
+		'f1': {'karyo': 'c1', 'start': 200, 'end': 400},
+		'f2': {'karyo': 'c2', 'start': 200, 'end': 400},
+		'f3': {'karyo': 'c2', 'start': 600, 'end': 800},
+		'f4': {'karyo': 'c3', 'start': 200, 'end': 400},
+		'f5': {'karyo': 'c4', 'start': 200, 'end': 400},
+		'f6': {'karyo': 'c4', 'start': 600, 'end': 800},
+		'f7': {'karyo': 'c3', 'start': 600, 'end': 800},
+		'f8': {'karyo': 'c4', 'start': 50, 'end': 100},
+		'f9': {'karyo': 'c5', 'start': 100, 'end': 300},
+		'f10': {'karyo': 'c6', 'start': 100, 'end': 150}
+};
 var links = {
             	 "l1": {'source': 'f1', 'target': 'f2', 'identity': 90}
 			 };
@@ -340,6 +377,14 @@ var links11 = {
 var links12 = {
 		"l1": {'source': 'f1', 'target': 'f2', 'identity': 75},
 		"l2": {'source': 'f3', 'target': 'f4', 'identity': 100}
+};
+
+var links13 = {
+		"l1": {'source': 'f1', 'target': 'f4', 'identity': 30},
+		"l2": {'source': 'f2', 'target': 'f5', 'identity': 99},
+		"l3": {'source': 'f3', 'target': 'f6', 'identity': 88},
+		"l4": {'source': 'f7', 'target': 'f9', 'identity': 77},
+		"l5": {'source': 'f8', 'target': 'f10', 'identity': 100}
 };
 
 
