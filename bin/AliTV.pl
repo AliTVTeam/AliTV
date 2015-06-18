3025c4723f6482eff8b8a98864c09430bc853b30
Markus Ankenbrand <markus@ankenbrand.me>
2015-06-18 14:13:05 +0200

    Added basic structure for the perl script including POD and first parameter

--- bin/AliTV.pl
+++ bin/AliTV.pl
@@ -1,3 +1,50 @@
+#!/usr/bin/perl
 use strict;
 use warnings;
 use Getopt::Long;
+use Pod::Usage;
+
+my %options;
+
+=head1 NAME
+
+AliTV.pl
+
+=head1 DESCRIPTION
+
+Wrapper to produce whole genome alignments and visualizations.
+
+=head1 USAGE
+
+  $ perl AliTV.pl --fasta human=>human.fa,mouse=>mouse.fa
+
+=head1 OPTIONS
+
+=over 25
+
+=item --fasta <GENOME>=><FASTA>[,<GENOME>=><FASTA>...]
+
+A comma separated list of genome, fasta pairs. Each genome is separated from its fasta file path by =>
+
+=cut
+
+$options{'fasta|f=s'} = \( my $opt_man );
+
+=back
+
+
+=head1 CODE
+
+=cut
+
+GetOptions(%options) or pod2usage(1);
+
+
+=head1 AUTHORS
+
+=over
+
+=item * Markus Ankenbrand, markus.ankenbrand@uni-wuerzburg.de
+
+=back
+
