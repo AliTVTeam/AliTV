c8ba2dfa7ffa9faf7ba6c9e18e243a73cd427a13
Markus Ankenbrand <markus@ankenbrand.me>
2015-06-19 10:04:32 +0200

    added creation of bed and tsv files to processing script

--- t/data/dataset001/process_testdata.sh
+++ t/data/dataset001/process_testdata.sh
@@ -9,3 +9,5 @@ do
 	fi
     done
 done
+
+cat *.txt | perl -F"\t" -ane 'BEGIN{$f=0;}next if(/^#/);print STDERR "$F[0]\t$F[1]\t$F[2]\tf$f\n"; $f++; ($F[5],$F[6])=($F[6],$F[5]) if($F[7] eq "-"); print STDERR "$F[4]\t$F[5]\t$F[6]\tf$f\n"; $f++; printf("f%i\tf%i\t%.2f\n", $f-2, $f-1, $F[10])' >link.tsv 2>link.bed
